7f2731269cfdb0ce28b33ece633df20d05201a20
GJ <coder_wugaojun@163.com>
2015-05-03 12:13:02 +0800

    添加类GJAutoCycleScrollView

---
+++ GJAutoCycleScrollViewSample/GJAutoCycleScrollView/GJAutoCycleScrollView.m
@@ -0,0 +1,31 @@
+//
+//  GJAutoCycleScrollView.m
+//  GJAutoCycleScrollViewSample
+//
+//  Created by imooc_gj on 15/5/3.
+//  Copyright (c) 2015年 devgj. All rights reserved.
+//
+
+#import "GJAutoCycleScrollView.h"
+
+@implementation GJAutoCycleScrollView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
