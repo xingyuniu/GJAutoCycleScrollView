d3e2be84ee65dc1c60d7944089d907689df20bd1
GJ <coder_wugaojun@163.com>
2015-05-03 12:04:00 +0800

    解决删除文件引起的错误

---
+++ GJAutoCycleScrollViewSample/GJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GJAppDelegate.h
+//  GJAutoCycleScrollView
+//
+//  Created by devgj on 15/5/3.
+//  Copyright (c) 2015年 devgj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GJAutoCycleScrollViewSample/GJViewController.h
@@ -0,0 +1,13 @@
+//
+//  GJViewController.h
+//  GJAutoCycleScrollView
+//
+//  Created by devgj on 15/5/3.
+//  Copyright (c) 2015年 devgj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GJViewController : UIViewController
+
+@end
